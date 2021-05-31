111f17ab3f3b452e8975c18ee6ed665ea30242a8
Shriya <shriya1130@gmail.com>
2021-05-31 18:15:53 +0530

    Reverted commits

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -17,9 +18,14 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use("/api/v1/transactions", transactions);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
